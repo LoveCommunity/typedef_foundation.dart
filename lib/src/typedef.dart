65cafdf1827a44ccf865afb1e81e07db39f63bcc
beeth0ven <beeth0vendev@gmail.com>
2022-01-27 09:08:36 +0800

    add `Getter` and `Setter`

---
+++ lib/src/typedef.dart
@@ -0,0 +1,6 @@
+
+/// typedef for getter
+typedef Getter<T> = T Function();
+
+/// typedef for setter
+typedef Setter<T> = void Function(T value);
--- test/typedef_foundation_test.dart
+++ test/typedef_foundation_test.dart
@@ -3,5 +3,21 @@ import 'package:test/test.dart';
 import 'package:typedef_foundation/typedef_foundation.dart';
 
 void main() {
-  
+
+  test('getter', () {
+
+    final Getter<int> getter = () => 0;
+    expect(getter(), 0);
+  });
+
+  test('setter', () {
+    
+    int _value = 0;
+    final Setter<int> setter = (value) {
+      _value = value;
+    };
+    expect(_value, 0);
+    setter(1);
+    expect(_value, 1);
+  });
 }
